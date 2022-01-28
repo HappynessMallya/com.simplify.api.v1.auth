5ab883fd2e925cce209acb1a5fc4439b7d2409d4
Angel Lozada <angel.lozada@koperca.com>
2022-01-28 10:46:17 -0400

    SIM-697: Added some minor improvements on dockerization

--- deploy.sh
+++ deploy.sh
@@ -19,17 +19,16 @@ docker-compose --file ${FILE_DOCKER_COMPOSE} up -d
 echo "*** Containers are running on production mode ***"
 docker-compose --file ${FILE_DOCKER_COMPOSE} ps
 
-echo "*** Starting create db in production mode +++"
-docker-compose --file ${FILE_DOCKER_COMPOSE} exec php-fpm php bin/console doctrine:database:create --if-not-exists
+echo "*** Starting create db in production mode ***"
+docker-compose --file ${FILE_DOCKER_COMPOSE} exec app php bin/console doctrine:database:create --if-not-exists
 echo "*** Command executed successfully ***"
 
 if [ "ls migrations/ | wc -l" > 1 ]; then
   echo "*** Run migrations db ***"
-  docker-compose --file ${FILE_DOCKER_COMPOSE} exec php-fpm php bin/console doctrine:migration:migrate --no-interaction
+  docker-compose --file ${FILE_DOCKER_COMPOSE} exec app php bin/console doctrine:migration:migrate --no-interaction
   echo "*** Migrations executed in database ***"
 else
   echo "*** Do not migrations to execute ***"
 fi
 
-## permissions command here? chmod -R 777 ...
 sudo chmod -R 777 var/
