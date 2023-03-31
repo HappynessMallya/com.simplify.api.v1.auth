518df967223a91d300fbf05beed1cc2fcc506615
Angel Lozada <angel.lozada@koperca.com>
2023-03-31 12:39:47 -0400

    SIM-684: Added new endpoint to change password validating with the current password

--- src/Domain/Services/User/PasswordEncoder.php
+++ src/Domain/Services/User/PasswordEncoder.php
@@ -5,10 +5,29 @@ declare(strict_types=1);
 namespace App\Domain\Services\User;
 
 use App\Domain\Model\User\User;
+use App\Infrastructure\Symfony\Security\UserEntity;
 
+/**
+ * Interface PasswordEncoder
+ * @package App\Domain\Services\User
+ */
 interface PasswordEncoder
 {
+    /**
+     * @return string|null
+     */
     public function getSalt(): ?string;
 
+    /**
+     * @param User $user
+     * @return string|null
+     */
     public function hashPassword(User $user): ?string;
+
+    /**
+     * @param UserEntity $user
+     * @param string $raw
+     * @return bool
+     */
+    public function isPasswordValid(UserEntity $user, string $raw): bool;
 }
